b92d098e8784d59b349bcdb3f3e51376046993e3
Meer Humza <meer.humza@profitoptics.com>
2023-11-20 13:49:56 +0500

    changing static paths

--- backend/test_44525/settings.py
+++ backend/test_44525/settings.py
@@ -182,7 +182,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'web_build')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'web_build', 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'web_build')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/mediafiles/'
